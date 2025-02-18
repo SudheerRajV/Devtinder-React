bf303703685ad9ee7eadef2296639b2ed5b238dc
Sudheer Varada <sudheervarada@Sudheers-MacBook-Pro.local>
2025-02-18 19:49:59 +0530

    Implemeted Router.

---
+++ src/components/Body.tsx
@@ -0,0 +1,15 @@
+import NavBar from './NavBar'
+import { Outlet } from 'react-router'
+import Footer from './Footer'
+
+const Body = () => {
+  return (
+    <>
+        <NavBar/>
+        <Outlet/>
+        <Footer/>
+    </>
+  )
+}
+
+export default Body
